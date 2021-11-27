5e029c52195ffef3e4e553d84dd4600d313f5ea4
bhimsenjoshi <bhimsen.joshi@gmail.com>
2021-11-27 06:08:17 +0530

    Directed and Path & count steps

--- BFS.py
+++ BFS.py
@@ -1,10 +1,26 @@
-def bfs(g, s):
-    que = [s]
-    while len(que) > 0:
-        current = que.pop(0)
-        print(current)
-        for neighbour in g[current]:
-            que.append(neighbour)
+def bfs(g, s, d):
+    if s == d:
+        print("Destination Reached")
+        return
+
+    frontier = [(s, 0)]
+    explored = []
+    path = {s: s}
+
+    while len(frontier) > 0:
+        node = frontier.pop(0)
+        explored.append(node)
+        print("Current {} required {}".format(node, d))
+        for neighbour in g[node[0]]:
+            # Check if this already explored
+            if (neighbour, node[1]) not in explored or frontier:
+                if neighbour == d:
+                    print("Destination Reached, cost = {}, Path is {}".format((node[1]+1), path[node[0]]))
+                    return
+                # else append the frontier
+                frontier.append((neighbour, (node[1]+1)))
+                path[neighbour] = path[node[0]] + neighbour
+            # If already explored then skip
 
 
 g1 = {'a': ['b', 'c'],
@@ -15,4 +31,4 @@ g1 = {'a': ['b', 'c'],
       'f': []
       }
 
-bfs(g1, 'a')
+bfs(g1, 'a', 'e')
