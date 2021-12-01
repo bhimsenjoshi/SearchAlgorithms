e399a34146a57c379d7a17ea681c6a7f70373644
bhimsenjoshi <bhimsen.joshi@gmail.com>
2021-12-01 23:10:17 +0530

    Undirected cyclic path Uniform cost search with minimum cost and paths

--- UniformCostSearch.py
+++ UniformCostSearch.py
@@ -1,35 +1,63 @@
-# TBD .
+# Uniform Cost Search.
+# Undirected cyclic graph
+# Bhimsen Joshi / Hyderabad / India
+# 1st Dec 2021
 
-
+# Cost of routes
 map1 = {('s', 'f'): 99,
         ('f', 'b'): 211,
         ('s', 'r'): 80,
         ('r', 'p'): 97,
-        ('p', 'b'): 101
+        ('p', 'b'): 101,
+        ('f', 's'): 99,
+        ('b', 'f'): 211,
+        ('r', 's'): 80,
+        ('p', 'r'): 97,
+        ('b', 'p'): 101
         }
 
+# Possible lists
+# This can be generated as well
 adjlst = {'s': ['f', 'r'],
-          'f': ['b'],
-          'r': ['p'],
-          'p': ['b'],
-          'b': []
+          'f': ['b', 's'],
+          'r': ['p', 's'],
+          'p': ['b', 'r'],
+          'b': ['f', 'p']
           }
 
-
+# Function / Algorithm
+# feed in the start and end location
+# returns all possible path with cost
 def UCS(s, e):
     frontier = [(0, s, '')]
     explored = []
+    exploredNode = []
+    result = []
     while len(frontier) > 0:
+
+        #sort as per minimum cost
         frontier.sort()
+        #popout the minimum cost node
         current = frontier.pop(0)
         explored.append(current)
+        exploredNode.append(current[1])
+
+        # Reached the goal
         if current[1] == e:
-            print("Goal Reached {}".format(current))
+            result.append(current)
+
+        # search all the neighbours
         for neighbour in adjlst[current[1]]:
+            # ignore if already explored
+            if neighbour in exploredNode:
+                continue
+            # compute the cost of reaching this node
             cost = map1[(current[1], neighbour)] + current[0]
+            # add it to frontier as we need to explore
             frontier.append((cost, neighbour, current[2]+current[1]))
-        #print(frontier)
-    return
+
+    # return all possible results
+    return result
 
 
-UCS('s', 'b')
+print(UCS('f', 'p'))
